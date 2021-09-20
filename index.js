b45111d1a301e1be7f4883b349dae0062a1f7587
max anselmo <itmanselmo@gmail.com>
2021-09-20 16:58:35 -0400

    commit tree 464b6b479f3519ba8e0302ceb341327f2c1ec555

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(value) {
+    return Math.abs(42 - value);
+}
+
+function distanceFromHqInFeet(value) {
+    let distanceInBlocks = distanceFromHqInBlocks(value);
+    return distanceInBlocks * 264;
+}
+
+function distanceTravelledInFeet(block1, block2) {
+    let blocks = Math.abs(block1 - block2);
+    return blocks * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    let cost = 0;
+    if(distance <= 400){
+        return cost;
+    }
+    else if(distance > 400 && distance <= 2000) {
+        distance = distance - 400;
+        cost = distance * 0.02;
+        return cost;
+    } else if(distance > 2000 && distance <= 2500) {
+        cost = 25;
+        return cost;
+    } else if(distance > 2500) {
+        return 'cannot travel that far';
+    }
+}
